66be0c2c0956e14bfdca33597c0ef3ff213b8e40
Dmitry Chaykin <hhdevr@gmail.com>
2025-11-28 01:01:04 +0900

    homework 4

--- payment-service-app/src/main/resources/data.sql
+++ payment-service-app/src/main/resources/data.sql
@@ -1,14 +1,14 @@
-insert into payment (amount, status, description, created_at, updated_at)
-values (10.00, 'PENDING', 'Test payment 1', current_timestamp(), current_timestamp());
+insert into payment (amount, description, created_at, updated_at)
+values (10.00, 'Test payment 1', current_timestamp(), current_timestamp());
 
-insert into payment (amount, status, description, created_at, updated_at)
-values (20.50, 'COMPLETED', 'Test payment 2', current_timestamp(), current_timestamp());
+insert into payment (amount, description, created_at, updated_at)
+values (20.50, 'Test payment 2', current_timestamp(), current_timestamp());
 
-insert into payment (amount, status, description, created_at, updated_at)
-values (30.00, 'REJECTED', 'Test payment 3', current_timestamp(), current_timestamp());
+insert into payment (amount, description, created_at, updated_at)
+values (30.00,  'Test payment 3', current_timestamp(), current_timestamp());
 
-insert into payment (amount, status, description, created_at, updated_at)
-values (40.75, 'CANCELED', 'Test payment 4', current_timestamp(), current_timestamp());
+insert into payment (amount, description, created_at, updated_at)
+values (40.75, 'Test payment 4', current_timestamp(), current_timestamp());
 
-insert into payment (amount, status, description, created_at, updated_at)
-values (99.99, 'PENDING', 'Test payment 5', current_timestamp(), current_timestamp());
+insert into payment (amount, description, created_at, updated_at)
+values (99.99,  'Test payment 5', current_timestamp(), current_timestamp());
